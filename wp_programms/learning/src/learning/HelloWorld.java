174b455458c8e8badd786d455b8425b263243715
Mehmet Kaya <mkaya@outlook.de>
2018-11-11 10:27:11 +0100

    random number

--- wp_programms/learning/src/learning/HelloWorld.java
+++ wp_programms/learning/src/learning/HelloWorld.java
@@ -1,10 +1,28 @@
 package learning;
 
+import java.util.Random;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hello");
+		
+		int randomNumber;
+		for(int i =0; i< 100; i++) {
+			randomNumber = (int)(Math.random()*10);
+			System.out.println(randomNumber);
+		}
+		
+		System.out.println("und so");
+		Random r = new Random();
+		for(int i =0; i< 100; i++) {
+			randomNumber = r.nextInt(11);
+			System.out.println(randomNumber);
+		}
+		
+		
+
 
 	}
 
